c0b1c888ca4f97e64fd281d24781eaab19c804de
Sergej Lozikov <sergej.lozikov@xpansa.com>
2017-10-17 21:20:02 +0200

    [UPD] re-order state selection

--- merp_inventory/models/stock_inventory.py
+++ merp_inventory/models/stock_inventory.py
@@ -6,11 +6,14 @@ from openerp import api, fields, models, _
 
 class Inventory(models.Model):
     _inherit = "stock.inventory"
-    _description = "Inventory"
 
-    state = fields.Selection(selection_add=[
-        ('ready', 'Waiting for Validation')
-    ])
+    def __init__(self, pool, cr):
+        init_res = super(Inventory, self).__init__(pool, cr)
+        selection_add = ('ready', 'Waiting for Validation')
+        if selection_add not in self.INVENTORY_STATE_SELECTION:
+            # add new state 'ready' before 'done'
+            type(self).INVENTORY_STATE_SELECTION.insert(-1, selection_add)
+        return init_res
 
     @api.multi
     def finish_inventory(self):
