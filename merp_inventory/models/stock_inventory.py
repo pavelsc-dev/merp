7d2fe4c9ef6d81f20dfceff4d5cfbede295329f7
Sergej Lozikov <sergej.lozikov@xpansa.com>
2017-10-18 06:19:42 +0200

    [UPD] re-order state selection

--- merp_inventory/models/stock_inventory.py
+++ merp_inventory/models/stock_inventory.py
@@ -2,15 +2,20 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import api, fields, models, _
+import logging
+_logger = logging.getLogger(__name__)
 
 
 class Inventory(models.Model):
     _inherit = "stock.inventory"
-    _description = "Inventory"
 
-    state = fields.Selection(selection_add=[
-        ('ready', 'Waiting for Validation')
-    ])
+    def _setup_base(self, partial):
+        init_res = super(Inventory, self)._setup_base(partial)
+        selection_add = ('ready', 'Waiting for Validation')
+        if selection_add not in self._fields['state'].selection:
+            # add new state 'ready' before 'done'
+            self._fields['state'].selection.insert(-1, selection_add)
+        return init_res
 
     @api.multi
     def finish_inventory(self):
