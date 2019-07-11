10b318174a22f3fd597999c66ec80b3948f61c2d
Denis Valenchyts <denis@ventor.tech>
2019-07-11 15:08:19 +0300

    [REF] Bug fixes in unit tests of the merp_outgoing_routing module

--- merp_outgoing_routing/tests/test_merp_outgoing_routing.py
+++ merp_outgoing_routing/tests/test_merp_outgoing_routing.py
@@ -45,7 +45,6 @@ class TestMerpOutgoingRouting(TransactionCase):
             'company_id': company.id,
             'picking_type_id': picking_type.id
         })
-        product_uom =company.currency_id
         products = self.env['product.template'].search([], limit=4)
         self.move_line_1 = self.env['stock.move.line'].create({
             'picking_id': self.stock_picking.id,
@@ -54,7 +53,7 @@ class TestMerpOutgoingRouting(TransactionCase):
             'date': datetime.now(),
             'location_dest_id': self.location_2.id,
             'product_uom_qty': 20.0,
-            'product_uom_id': product_uom.id,
+            'product_uom_id': products[0].uom_id.id,
             'product_id': products[0].id
         })
         self.move_line_2 = self.env['stock.move.line'].create({
@@ -64,7 +63,7 @@ class TestMerpOutgoingRouting(TransactionCase):
             'date': datetime.now(),
             'location_dest_id': self.location_3.id,
             'product_uom_qty': 25.0,
-            'product_uom_id': product_uom.id,
+            'product_uom_id': products[1].uom_id.id,
             'product_id': products[1].id
         })
         self.move_line_3 = self.env['stock.move.line'].create({
@@ -74,7 +73,7 @@ class TestMerpOutgoingRouting(TransactionCase):
             'date': datetime.now(),
             'location_dest_id': self.location_1.id,
             'product_uom_qty': 15.0,
-            'product_uom_id': product_uom.id,
+            'product_uom_id': products[2].uom_id.id,
             'product_id': products[2].id
         })
         self.move_line_4 = self.env['stock.move.line'].create({
@@ -84,7 +83,7 @@ class TestMerpOutgoingRouting(TransactionCase):
             'date': datetime.now(),
             'location_dest_id': self.location_2.id,
             'product_uom_qty': 10.0,
-            'product_uom_id': product_uom.id,
+            'product_uom_id': products[3].uom_id.id,
             'product_id': products[3].id
         })
 
