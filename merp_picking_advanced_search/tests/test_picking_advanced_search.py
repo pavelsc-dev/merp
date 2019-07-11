637b3ac4c2d58c3354222f9a88e66e6ec6daee9d
Denis Valenchyts <denis@ventor.tech>
2019-07-11 18:03:01 +0300

    [REF] Bug fixes

--- merp_picking_advanced_search/tests/test_picking_advanced_search.py
+++ merp_picking_advanced_search/tests/test_picking_advanced_search.py
@@ -43,6 +43,7 @@ class TestPickingAdvancedSearch(TransactionCase):
             'company_id': company.id,
             'picking_type_id': picking_type.id
         })
+        product_uom = self.env['uom.uom'].search([], limit=1, order='id')
         products = self.env['product.template'].search([], limit=3)
         self.move_line_1 = self.env['stock.move.line'].create({
             'picking_id': self.stock_picking_1.id,
@@ -51,7 +52,7 @@ class TestPickingAdvancedSearch(TransactionCase):
             'date': datetime.now(),
             'location_dest_id': self.location_2.id,
             'product_uom_qty': 20.0,
-            'product_uom_id': 1,
+            'product_uom_id': product_uom.id,
             'product_id': products[0].id
         })
         self.move_line_2 = self.env['stock.move.line'].create({
@@ -61,7 +62,7 @@ class TestPickingAdvancedSearch(TransactionCase):
             'date': datetime.now(),
             'location_dest_id': self.location_1.id,
             'product_uom_qty': 25.0,
-            'product_uom_id': 1,
+            'product_uom_id': product_uom.id,
             'product_id': products[1].id
         })
         self.move_line_3 = self.env['stock.move.line'].create({
@@ -71,7 +72,7 @@ class TestPickingAdvancedSearch(TransactionCase):
             'date': datetime.now(),
             'location_dest_id': self.location_1.id,
             'product_uom_qty': 15.0,
-            'product_uom_id': 1,
+            'product_uom_id': product_uom.id,
             'product_id': products[2].id
         })
         self.move_line_4 = self.env['stock.move.line'].create({
@@ -81,7 +82,7 @@ class TestPickingAdvancedSearch(TransactionCase):
             'date': datetime.now(),
             'location_dest_id': self.location_2.id,
             'product_uom_qty': 15.0,
-            'product_uom_id': 1,
+            'product_uom_id': product_uom.id,
             'product_id': products[1].id
         })
 
