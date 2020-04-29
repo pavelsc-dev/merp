75420dc4da5be8cecc572a52c1c10c8f620f353e
Denis <denis@ventor.tech>
2020-04-29 19:17:31 +0300

    [IMP] MERPWH-603: Added constraints for type in barcodes

--- product_multiple_barcodes/models/product_barcode_multi.py
+++ product_multiple_barcodes/models/product_barcode_multi.py
@@ -1,7 +1,8 @@
 # Copyright 2019 VentorTech OU
 # Part of Ventor modules. See LICENSE file for full copyright and licensing details.
 
-from odoo import models, fields
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 
 class ProductBarcodeMulti(models.Model):
@@ -9,4 +10,15 @@ class ProductBarcodeMulti(models.Model):
     _description = 'Product Barcode Multi'
 
     name = fields.Char('Barcode')
-    product_id = fields.Many2one('product.product', string='Product')
+    product_id = fields.Many2one(
+        'product.product', 
+        string='Product', 
+        required=True,
+        ondelete="cascade",
+    )
+
+    @api.constrains('name')
+    def _check_empty_name(self):
+        for rec in self:
+            if not rec.name:
+                raise ValidationError("Additional barcode can't be empty")
