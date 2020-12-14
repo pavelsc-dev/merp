2506715e8c8e2378f12db33bc277c4c6b8478857
Vladimir Natsevsky <vladimir.n@ventor.tech>
2020-12-14 14:09:47 +0300

    init

--- merp_default_locations/tests/__init__.py
+++ merp_default_locations/tests/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2019 VentorTech OU
-# Part of Ventor modules. See LICENSE file for full copyright and licensing details.
+# Copyright 2020 VentorTech OU
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0).
 
 from . import test_check_default_location
