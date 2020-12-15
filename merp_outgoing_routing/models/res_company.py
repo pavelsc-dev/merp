7f05b8d5a06f3a1c1c736090a79d96e39219ebf5
Vladimir Natsevsky <vladimir.n@ventor.tech>
2020-12-15 15:32:05 +0300

    ini

---
+++ merp_outgoing_routing/models/res_company.py
@@ -0,0 +1,32 @@
+﻿# Copyright 2019 VentorTech OU
+# Part of Ventor modules. See LICENSE file for full copyright and licensing details.
+
+from odoo import models, fields
+
+
+class Company(models.Model):
+    _inherit = 'res.company'
+
+    outgoing_routing_strategy = fields.Selection(
+        [
+            # path should be valid for both stock pickings and quants
+            ('location_id.removal_prio', 'Location removal priority'),
+            ('location_id.name', 'Location name'),
+            ('product_id.name', 'Product name'),
+        ],
+        string='Picking Strategy', default='location_id.name')
+
+    outgoing_routing_order = fields.Selection(
+        [
+            ('0', 'Ascending (A-Z)'),
+            ('1', 'Descending (Z-A)'),
+        ],
+        string='Picking Order', default='0')
+
+    stock_reservation_strategy = fields.Selection(
+        [
+            ('base', 'Routing Strategy'),
+            ('quantity', 'By Quantity'),
+            ('none', 'Default'),
+        ],
+        string='Reservation Strategy', default='base')
---
+++ merp_outgoing_routing/models/stock_pack_operation.py
@@ -0,0 +1,17 @@
+﻿# Copyright 2019 VentorTech OU
+# Part of Ventor modules. See LICENSE file for full copyright and licensing details.
+
+from odoo import models, api
+
+
+class StockPackOperation(models.Model):
+    _inherit = 'stock.move.line'
+
+    @api.model
+    def _compute_operation_valid(self):
+        res = True
+        if hasattr(super(StockPackOperation, self), '_compute_operation_valid'):
+            res &= super(StockPackOperation, self)._compute_operation_valid()
+        res &= self.qty_done != self.product_qty
+        # res &= self.state not in ('draft', 'cancel', 'done')
+        return res
